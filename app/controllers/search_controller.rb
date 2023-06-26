5943155ac7871692b452e731b749d1837f4cbcd6
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-06-26 13:28:36 -0600

    define index method

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,16 @@
 class SearchController < ApplicationController
   def index
+    state = params[:state]
+
+    conn = Faraday.new(url: "https://api.propublica.org") do |faraday|
+      faraday.headers["X-API-Key"] = 'S9JON3ruNOI6XiyymcnZ7gtsjnToPxuXyT0bgeaX'
+    end
+
+    response = conn.get("/congress/v1/members/house/co/current.json")
+    # require 'pry'; binding.pry
+
+    json = JSON.parse(response.body, symbolize_names: true)
+    @members = json[:results]
     
   end
 end
