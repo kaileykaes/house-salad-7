95a2ae4eca4e7b208cced5ec8f371e13efa82831
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-06-27 16:22:10 -0600

    add missing expect statement

--- spec/features/user_can_search_by_state_spec.rb
+++ spec/features/user_can_search_by_state_spec.rb
@@ -6,6 +6,7 @@ feature "user can search for house members" do
     select "Colorado", from: :state
     click_on "Locate Members of the House"
     expect(current_path).to eq(search_path)
+    expect(page).to have_content("7 Results")
     expect(page).to have_css(".member", count: 8)
 
     within(first(".member")) do 
