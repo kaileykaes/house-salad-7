a6be3eb5637bee2e22e7199caf370dcb574643bc
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-06-27 16:25:02 -0600

    use ENV variable rather than API key in controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,14 +3,12 @@ class SearchController < ApplicationController
     state = params[:state]
 
     conn = Faraday.new(url: "https://api.propublica.org") do |faraday|
-      faraday.headers["X-API-Key"] = 'S9JON3ruNOI6XiyymcnZ7gtsjnToPxuXyT0bgeaX'
+      faraday.headers["X-API-Key"] = ENV["PROPUBLICA_API_KEY"]
     end
 
     response = conn.get("/congress/v1/members/house/co/current.json")
-    # require 'pry'; binding.pry
 
     json = JSON.parse(response.body, symbolize_names: true)
     @members = json[:results]
-    
   end
 end
